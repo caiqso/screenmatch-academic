32e93c79dea1ee98bb540f42b594a365c1fed651
Caíque <caaique.c2@gmail.com>
2026-04-13 19:06:48 -0300

    aprimorando o projeto explorando streams

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,12 +3,13 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Principal {
 
@@ -38,14 +39,40 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
-//        for (int i = 0; i < dados.totalTemporadas() ; i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j < episodiosTemporada.size() ; j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
+        for (int i = 0; i < dados.totalTemporadas() ; i++) {
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size() ; j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 melhores episódios da série " + nomeSerie);
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+//        List<String> nomes = Arrays.asList("Caique", "Verlotta", "Levi", "Kaue", "Felipe");
+//
+//        nomes.stream()
+//                .sorted()
+//                .limit(5)
+//                .filter(n -> n.toLowerCase().startsWith("v"))
+//                .forEach(System.out::println);
 
 
     }
